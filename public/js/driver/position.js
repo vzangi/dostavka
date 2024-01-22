868176897bfa788f301a5ab4e075d274218db8d0
Vladimir <vzangi@yandex.ru>
2024-01-22 15:02:25 +0300

    whatch

--- public/js/driver/position.js
+++ public/js/driver/position.js
@@ -1,29 +1,21 @@
 $(function () {
   // Если браузер позволяет определить местоположение
   if ('geolocation' in navigator) {
-    getPosition()
-  }
-
-  function getPosition() {
     const options = {
       enableHighAccuracy: false,
     }
-    navigator.geolocation.getCurrentPosition(
-      successCallback,
-      errorCallback,
-      options
-    )
-
-    setTimeout(getPosition, 5000)
+    navigator.geolocation.watchPosition(successCallback, errorCallback, options)
   }
 
   function successCallback(position) {
     const { coords } = position
     const { latitude, longitude } = coords
+
+    console.log(latitude, longitude)
     // Сохраняю текущую позицию
     socket.emit('driver.setposition', { latitude, longitude })
 
-    $('.driver-wallet').parent().text(`${latitude};${longitude}`)
+    $('h2').toggleClass('bg-danger')
   }
 
   function errorCallback(error) {
